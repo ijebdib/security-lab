9ae5d2011f8bd395a20446bc57394206c96fa0f1
ijebdib <ijebdib@gmail.com>
2026-05-15 16:00:57 +0800

    Update app.js
    Fix SQL injection using parameterized query

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const pool = new pg.Pool();
 app.get("/user", async (req, res) => {
   const username = req.query.username;
 
-  const query = "SELECT * FROM users WHERE username = '" + username + "'";
+  const query = "SELECT * FROM users WHERE username = $1";
 
-  const result = await pool.query(query);
+ const result = await pool.query(query, [username]);
   res.json(result.rows);
 });
 
